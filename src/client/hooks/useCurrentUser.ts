6ccbe91705ebe33e104630c1d4d5f0d7cbd4442d
davigon <dvgnlz@outlook.es>
2022-06-17 03:42:24 +0200

    Add current user hook

---
+++ src/client/hooks/useCurrentUser.ts
@@ -0,0 +1,28 @@
+import { useQuery, UseQueryResult } from "react-query"
+import { CurrentUser } from "../../../types/types"
+import { useAuth } from "./useAuth"
+import { useLocalStorage } from "./useLocalStorage"
+
+export const useCurrentUser = () => {
+  const [session] = useLocalStorage("session", "")
+  const {isLoggedIn, isLoading} = useAuth()
+
+  const currentUserFetch = async () => {
+    const response = await fetch(`/api/user/currentUser`, {
+      method: "GET",
+      headers: { "Content-Type": "application/json", session },
+      credentials: "include",
+    })
+    return response.json()
+  }
+
+  const currentUserQuery: UseQueryResult<CurrentUser, Error> = useQuery(
+    ["currentUser"],
+    currentUserFetch,
+    {
+      enabled: isLoggedIn && !isLoading,
+    }
+  )
+
+  return { currentUserQuery }
+}
