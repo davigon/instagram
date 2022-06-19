9d8d55ee5ea59c6d9ebdd7fce8bc7d8e99ef5e38
davigon <dvgnlz@outlook.es>
2022-06-19 04:27:17 +0200

    Display stories on user page

--- src/client/pages/UserPage.tsx
+++ src/client/pages/UserPage.tsx
@@ -1,5 +1,14 @@
 import React, { useState } from "react"
-import { Box, Container, VStack } from "@chakra-ui/react"
+import {
+  Box,
+  Container,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+  VStack,
+} from "@chakra-ui/react"
 import { useUser } from "../hooks/useUser"
 import { useParams } from "react-router-dom"
 import { UserCard } from "../components/user/UserCard"
@@ -7,18 +16,42 @@ import { BasicPage } from "./BasicPage"
 import { Tray } from "../components/tray/Tray"
 import { useHighlightsTray } from "../hooks/useHighlightsTray"
 import { TrayItem } from "../components/tray/TrayItem"
+import { StoriesType, useStories } from "../hooks/useStories"
+import { ContentGrid } from "../components/content/ContentGrid"
+import { StoryCard } from "../components/content/StoryCard"
 
 export const UserPage = () => {
+  const [contentType, setContentType] = useState(0)
+  const [selectedTab, setSelectedTab] = useState(0)
+  const [selectedItem, setSelectedItem] = useState("")
+
   const { username } = useParams()
   const { userQuery } = useUser(String(username))
-  const { highlightsTray, isLoading } = useHighlightsTray(
+  const highlightsTray = useHighlightsTray(
     String(username),
     userQuery.data?.currentUserAllowedToView || false
   )
+  const stories = useStories(
+    String(username),
+    (userQuery.data?.currentUserAllowedToView && contentType === 1) || false,
+    StoriesType.Stories
+  )
+  const highlights = useStories(
+    selectedItem,
+    (userQuery.data?.currentUserAllowedToView && contentType === 2) || false,
+    StoriesType.Highlights
+  )
 
-  const [selectedItem, setSelectedItem] = useState("")
   const handleClickTrayItem = (id: string) => {
+    setContentType(2)
     setSelectedItem(id)
+    setSelectedTab(-1)
+  }
+
+  const handleChangeTab = (index: number) => {
+    setContentType(index)
+    setSelectedTab(index)
+    setSelectedItem("")
   }
 
   if (userQuery.isLoading || userQuery.data === undefined)
@@ -31,20 +64,64 @@ export const UserPage = () => {
           <UserCard user={userQuery.data} />
         </Container>
         <Container maxW="container.md">
-          <Tray isLoading={isLoading}>
-            {highlightsTray &&
-              highlightsTray.map((i) => {
-                return (
-                  <TrayItem
-                    key={i.id}
-                    itemId={i.id}
-                    item={i}
-                    onClick={handleClickTrayItem}
-                    active={selectedItem === i.id}
-                  />
-                )
-              })}
+          <Tray isLoading={highlightsTray.isLoading}>
+            {highlightsTray.highlightsTray?.map((i) => {
+              return (
+                <TrayItem
+                  key={i.id}
+                  itemId={i.id}
+                  item={i}
+                  onClick={handleClickTrayItem}
+                  active={selectedItem === i.id}
+                />
+              )
+            })}
           </Tray>
+        </Container>
+        <Container maxW="container.md">
+          <Tabs
+            isFitted
+            variant="enclosed"
+            index={selectedTab}
+            onChange={handleChangeTab}
+          >
+            <TabList mb="1em">
+              <Tab>Publicaciones</Tab>
+              <Tab>Historias</Tab>
+            </TabList>
+            <TabPanels display={contentType === 2 ? "none" : undefined}>
+              <TabPanel>
+                <p>Publicaciones</p>
+              </TabPanel>
+              <TabPanel>
+                <ContentGrid
+                  contentLength={stories.stories?.length || 0}
+                  hasMoreContent={false}
+                  loadMoreContent={() => {}}
+                  isLoadingContent={stories.isLoading}
+                >
+                  {stories.stories?.map((i) => {
+                    return <StoryCard key={i.id} story={i} />
+                  })}
+                </ContentGrid>
+              </TabPanel>
+            </TabPanels>
+          </Tabs>
+        </Container>
+        <Container
+          maxW="container.md"
+          display={contentType !== 2 ? "none" : undefined}
+        >
+          <ContentGrid
+            contentLength={highlights.stories?.length || 0}
+            hasMoreContent={false}
+            loadMoreContent={() => {}}
+            isLoadingContent={highlights.isLoading}
+          >
+            {highlights.stories?.map((i) => {
+              return <StoryCard key={i.id} story={i} />
+            })}
+          </ContentGrid>
         </Container>
       </VStack>
     </BasicPage>
